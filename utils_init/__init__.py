765a7157c74625b7404a0ac0b5222c96ea170713
JiHyun Kim <rlawlgus251@cau.ac.kr>
2024-05-13 01:09:46 +0900

    Create __init__.py

---
+++ utils_init/__init__.py
@@ -0,0 +1,12 @@
+"""Useful utils
+"""
+from .misc import *
+from .logger import *
+from .eval import *
+
+# progress bar
+import os, sys
+import time
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "progress"))
+from progress.bar import Bar as Bar
